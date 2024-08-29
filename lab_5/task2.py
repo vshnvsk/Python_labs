56ea17440d7ac69b61ae1accd80f1c809ca2deaa
vshnvsk <112852238+vshnvsk@users.noreply.github.com>
2024-08-29 17:44:26 +0300

    update task #2

--- lab_5/task2.py
+++ lab_5/task2.py
@@ -5,16 +5,19 @@ def func():
     user_str = str(input("Enter your string: \n"))
     data = []
     vocabulary = {}
+    count = 1
 
     for i in user_str.split():
         data.append(i)
 
     for word in data:
-        count = 1
         for symbol in word:
-            vocabulary[symbol] = count
-            if symbol in vocabulary.keys():
+            if symbol not in vocabulary.keys():
+                count = 1
+                vocabulary[symbol] = count
+            else:
                 count += 1
+                vocabulary[symbol] = count
 
     print(vocabulary)
 
